d1c0a7bc84a28c45a5dd2a098a4eb47d8754e30f
jose perez <josheperzng@gmail.com>
2022-09-06 17:53:19 -0400

    integrating jquery

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
+/*
+*vanilla*
+
 const mainGallery = document.querySelector('#main-gallery');
 const containerZoomGallery = document.querySelector('#container-zoom-gallery');
 const zoomGallery = document.createElement('div'); 
@@ -18,8 +21,18 @@ function insertarGalleryZoom() {
   containerZoomGallery.appendChild(zoomGallery);
   iconCloseZoomGallery.classList.remove('not-visible');
  
-};
+}; */
 
+/* jquery */
+$('#main-gallery').click(function(){
+  $('#container-zoom-gallery').append('<zoom-gallery></zoom-gallery>');
+ $('#icon-close').removeClass('not-visible');
+});
+
+$('#icon-close').click(function(){
+  $('#container-zoom-gallery').empty();
+  $('#icon-close').addClass('not-visible');
+});
 
 
 
